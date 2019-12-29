315c42f13755b5eb240a8d6f058f6cdd87f61f86
Jm <Jean.M.Girard@Outlook.com>
2019-12-28 21:03:05 -0500

    scss

--- src/app2.test.js
+++ src/app2.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App2 from './app2';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App2 />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
